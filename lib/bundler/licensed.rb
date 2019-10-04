3e486e95e0ef346957b04409d8789f5362987ab5
Sergey Alekseev <sergey@asoft.co>
2019-10-04 15:27:34 +0300

    Add Bundler hook to run licensed cache

--- lib/bundler/licensed.rb
+++ lib/bundler/licensed.rb
@@ -5,6 +5,9 @@ require 'bundler/licensed/version'
 module Bundler
   module Licensed
     class Error < StandardError; end
-    # Your code goes here...
+
+    Bundler::Plugin.add_hook('after-install-all') do
+      system 'licensed cache'
+    end
   end
 end
